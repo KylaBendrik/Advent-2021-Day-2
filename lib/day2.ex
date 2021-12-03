92cd0730644581302ada99edc436245c9653e73e
Beth Jackson <Mbeth.ashton@gmail.com>
2021-12-03 14:19:06 -0500

    gold star

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,18 +1,97 @@
 defmodule Day2 do
   @moduledoc """
-  Documentation for `Day2`.
+  forward x increases the horizontal position by x
+  down x increases the depth by x
+  up x decreases the depth by x
+  
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day2.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  
+  def run(filename, :stage1) do
+    # initialize position map: {h: int, d: int}
+    pos = initialize_position()
+    # run input_text through translator
+    moves = translate(filename)
+    # apply positional changes
+    new_pos = move(pos, moves)
+    # multiply final horizontal position by final depth
+    answer = new_pos.h * new_pos.d
+    IO.puts("answer: #{answer}")
+    answer
+  end
+  
+  def run(filename, :stage2) do
+    # initialize position map: {h: int, d: int}
+    pos = initialize_position()
+    # run input_text through translator
+    moves = translate(filename)
+    # apply positional changes
+    new_pos = move_aim(pos, moves)
+    # multiply final horizontal position by final depth
+    answer = new_pos.h * new_pos.d
+    IO.puts("answer (stage2): #{answer}")
+    answer
+  end
+  
+  # -- positional functions --
+  def initialize_position do
+    %{h: 0, d: 0, aim: 0}
+  end
+  
+  def forward(%{h: horizontal, d: depth}, amount) do
+    %{h: horizontal + amount, d: depth}
+  end
+  
+  def down(%{h: horizontal, d: depth}, amount) do
+    %{h: horizontal, d: depth + amount}
+  end
+  
+  def up(%{h: horizontal, d: depth}, amount) do
+    %{h: horizontal, d: depth - amount}
+  end
+  
+  def aim_forward(%{h: horizontal, d: depth, aim: aim}, amount) do
+    %{h: horizontal + amount, d: depth + (amount * aim), aim: aim}
+  end
+  
+  def aim_down(%{h: horizontal, d: depth, aim: aim}, amount) do
+    %{h: horizontal, d: depth, aim: aim + amount}
+  end
+  
+  def aim_up(%{h: horizontal, d: depth, aim: aim}, amount) do
+    %{h: horizontal, d: depth, aim: aim - amount}
+  end
+  
+  def move(pos, []) do
+    pos
+  end
+  def move(pos, [%{go: direction, amount: amount} | rest]) do
+    case direction do
+      "forward" ->  move(forward(pos, amount), rest)
+      "down" ->     move(down(pos, amount), rest)
+      "up" ->       move(up(pos, amount), rest)
+    end
+  end
+  
+  def move_aim(pos, []) do
+    pos
+  end
+  def move_aim(pos, [%{go: direction, amount: amount} | rest]) do
+    case direction do
+      "forward" ->  move_aim(aim_forward(pos, amount), rest)
+      "down" ->     move_aim(aim_down(pos, amount), rest)
+      "up" ->       move_aim(aim_up(pos, amount), rest)
+    end
+  end
+  
+  def translate(filename) do
+    # open file
+    {:ok, input} = File.read(filename)
+    
+    # turn it into a list by /r/n
+    String.split(input, ["\r", "\n", "\r\n"], trim: true)
+    # returns list of maps: {go: "forward", amount: 5}
+    |> Enum.map(fn x -> String.split(x, " ") end) 
+    |> Enum.map(fn [dir, a] -> %{go: dir, amount: String.to_integer(a)} end) 
   end
 end
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -2,7 +2,59 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "greets the world" do
-    assert Day2.hello() == :world
+  test "initialize_position returns a position map with both positions at 0" do
+    assert Day2.initialize_position().h == 0
+    assert Day2.initialize_position().d == 0
+  end
+  
+  test "forward 5 from 0,0" do
+    pos = Day2.initialize_position()
+    
+    assert Day2.forward(pos, 5) == %{h: 5, d: 0}
+  end
+  
+  test "down 5 from 0,0" do
+    pos = Day2.initialize_position()
+    
+    assert Day2.down(pos, 5) == %{h: 0, d: 5}
+  end
+  
+  test "up 5 from 0,0 (We're flying!)" do
+    pos = Day2.initialize_position()
+    
+    assert Day2.up(pos, 5) == %{h: 0, d: -5}
+  end
+  
+  test "multiple moves from list of move maps" do
+    pos = Day2.initialize_position()
+    moves = [
+      %{go: "down", amount: 5},
+      %{go: "forward", amount: 5},
+      %{go: "up", amount: 1}
+    ]
+    
+    assert Day2.move(pos, moves) == %{h: 5, d: 4}
+  end
+  
+  test "read example data into list of move maps" do
+    assert Day2.translate("lib/input/example_data.txt") == 
+    [%{go: "forward", amount: 5}, 
+    %{go: "down", amount: 5}, 
+    %{go: "forward", amount: 8}, 
+    %{go: "up", amount: 3}, 
+    %{go: "down", amount: 8}, 
+    %{go: "forward", amount: 2}]
+  end
+  
+  test "run example data" do
+    assert Day2.run("lib/input/example_data.txt", :stage1) == 150
+  end
+  
+  test "run actual data stage1" do
+    Day2.run("lib/input/data.txt", :stage1)
+  end
+  
+  test "run actual data stage2" do
+    Day2.run("lib/input/data.txt", :stage2)
   end
 end
